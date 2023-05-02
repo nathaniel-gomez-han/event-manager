83ff6c2692552af474318bbee225bd264d32b094
nathaniel-gomez-han <nathaniel.gomez.han@gmail.com>
2023-05-02 00:01:02 -0500

    Integrated Bootstrap 5.3 and added a basic navbar and footer.

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-    session_start();
+session_start();
 
-    $isUserValid = false;
+// Authentication
+$isUserValid = false;
+if (isset($_SESSION['isUserValid'])) {
+    $isUserValid = $_SESSION['isUserValid'];
+}
 
-    if (isset($_SESSION['isUserValid'])) {
-        $isUserValid = $_SESSION['isUserValid'];
-    }
+// Dynamic Page Content
+$currentYear = date("Y");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +16,7 @@
         <meta charset="UTF-8">
         <!--
             Author: Nathaniel Gomez-Han
-            Date: April 26 2023
+            Date: May 1 2023
         -->
         <meta http-equiv="X-UA-Compatible" content ="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,17 +28,52 @@
         <link rel="manifest" href="/manifest.webmanifest">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/main.css">
     </head>
-    <body>
-		<nav>
-            <div class="site-logo">
+    <body class="d-flex flex-column">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand h1 mb-lg-0" href="index.php">
+                <img src="img/rocktane-logo.svg" alt="Rocktane Logo" height="30">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tour</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        <main>
-            <h1>Event Manager</h1>
-            <h2>Upcoming Events:</h2>
+        </div>
+    </nav>
+    <!-- Page Content -->
+    <main class="flex-grow-1">
+        <h1>Event Manager</h1>
+        <h2>Upcoming Events:</h2>
+        
+    </main>
+    <!-- Footer -->
+    <footer class="text-center">
+        <!-- Copyright -->
+        <div class="text-center p-3 bg-dark text-bg-dark">
+            &copy; <?= $currentYear ?> Nathaniel Gomez-Han
+        </div>
+    </footer>
 
-        </main>
+    <!-- JavaScript -->
+    <script src="js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
