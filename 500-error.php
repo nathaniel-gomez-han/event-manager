908958ca991bfd2509ad900e3760dd6dab52fdd0
nathaniel-gomez-han <nathaniel.gomez.han@gmail.com>
2023-05-04 01:06:15 -0500

    Added 500 Error page.

---
+++ 500-error.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!--
+        Author: Nathaniel Gomez-Han
+    -->
+    <meta http-equiv="X-UA-Compatible" content ="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Manager</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" href="/favicon.ico" sizes="16x16 32x32"><!-- 32×32 -->
+    <link rel="icon" href="/icon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png"><!-- 180×180 -->
+    <link rel="manifest" href="/manifest.webmanifest">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.typekit.net/ukm4sma.css"><!-- Adobe Fonts (Gin) -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body class="d-flex flex-column min-vh-100">
+<!-- Navbar -->
+<?php include 'inc/navbar.php'; ?>
+
+<!-- Page Content -->
+<main class="d-flex flex-column flex-grow-1 p-0">
+    <section class="flex-grow-1 container-lg py-4 theme-bg-dark">
+        <h1 class="display-1">500 Error</h1>
+        <p>The server has encountered an error. Please try again soon.</p>
+    </section>
+</main>
+
+<!-- Footer -->
+<?php include 'inc/footer.php'; ?>
+
+<!-- JavaScript -->
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
