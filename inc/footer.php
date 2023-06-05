445387fa6b72c69fa44d9b998b31126fecccc316
nathaniel-gomez-han <nathaniel.gomez.han@gmail.com>
2023-06-05 09:47:18 -0500

    Added page links to the footer.

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,28 @@
 <?php
 $currentYear = date('Y');
 ?>
-<footer class="text-center mt-auto">
+<footer class="footer d-flex flex-column mt-auto">
+    <!-- Page Links -->
+    <ul class="footer-links nav align-self-center d-flex flex-column flex-sm-row py-3 text-center">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="tour-dates.php">Tour Dates</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Admin Login</a>
+        </li>
+    </ul>
+
     <!-- Copyright -->
-    <div class="text-center p-3 bg-dark text-bg-dark">
+    <div class="footer-copyright w-100 text-center py-2">
         &copy; <?= $currentYear ?> Nathaniel Gomez-Han
     </div>
 </footer>
